16f46ef9cd337539defc0fee5a4820b05542db6b
Cwaihenya <cwaihenya.gmail.com>
2021-06-16 17:10:45 +0900

    creation of user-id colun on blogs

---
+++ db/migrate/20210616075511_add_users_ref_blogs.rb
@@ -0,0 +1,5 @@
+class AddUsersRefBlogs < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :blogs, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_060449) do
+ActiveRecord::Schema.define(version: 2021_06_16_075511) do
 
   create_table "blogs", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "image"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema.define(version: 2021_06_16_060449) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "blogs", "users"
 end
